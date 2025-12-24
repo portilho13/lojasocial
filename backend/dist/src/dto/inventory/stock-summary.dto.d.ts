029afbf5dec34153c755ef007682341abd9c0314
brndz1 <digas9599@gmail.com>
2025-12-24 00:47:33 +0000

    Removed node_modules and dist from tracking

--- backend/dist/src/dto/inventory/stock-summary.dto.d.ts
+++
@@ -1,4 +0,0 @@
-export declare class StockSummaryDto {
-    category: string;
-    totalQuantity: number;
-}
--- backend/dist/src/dto/inventory/stock-summary.dto.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.StockSummaryDto = void 0;
-class StockSummaryDto {
-    category;
-    totalQuantity;
-}
-exports.StockSummaryDto = StockSummaryDto;
-//# sourceMappingURL=stock-summary.dto.js.map
