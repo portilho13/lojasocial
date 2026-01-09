bb7320a6a953aedf0aba1222202274404c0c3375
pdqwe2000 <pdqwe2000@gmail.com>
2026-01-09 00:12:19 +0000

    Update build.gradle.kts

--- mobile/app/build.gradle.kts
+++ mobile/app/build.gradle.kts
@@ -54,6 +54,7 @@ dependencies {
     implementation(libs.androidx.compose.material3)
     implementation(libs.androidx.compose.runtime)
     implementation(libs.androidx.compose.foundation)
+    implementation(libs.androidx.compose.foundation.layout)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
