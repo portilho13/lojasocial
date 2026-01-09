ef2a79d6e72c87844d8119ccc2f8e2ac43a4568d
TIAGO\tiago <a27675@alunos.ipca.pt>
2026-01-09 01:48:58 +0000

    update

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,22 +1,26 @@
 import { Module } from '@nestjs/common';
-import { AuthController } from './controller/auth.controller';
-import { PrismaModule } from 'prisma/prisma.module';
 import { JwtModule } from '@nestjs/jwt';
+import { PrismaModule } from 'prisma/prisma.module';
 import { InventoryModule } from './inventory.module';
 import { SupportRequestModule } from './support-request.module';
 import { BeneficiaryModule } from './beneficiary.module';
-
+import { DonationModule } from './donation.module';
+import { AuthController } from './controller/auth.controller';
+import { AppointmentController } from './controller/appointment.controller';
 import { StudentRepository } from './repository/student.repository';
 import { UserRepository } from './repository/user.repository';
-
+import { AppointmentsRepository } from './repository/appointments.repository';
 import { StudentService } from './service/student.service';
 import { UserService } from './service/user.service';
+import { EmailService } from './service/email.service';
+import { AppointmentService } from './service/appointment.service';
 import { AccessTokenStrategy } from './auth/strategies/access-token.strategy';
 import { RefreshTokenStrategy } from './auth/strategies/refresh-token.strategy';
 
 const repositories = [
   StudentRepository,
   UserRepository,
+  AppointmentsRepository,
 ];
 
 const services = [
@@ -24,6 +28,13 @@ const services = [
   UserService,
   AccessTokenStrategy,
   RefreshTokenStrategy,
+  EmailService,
+  AppointmentService,
+];
+
+const controllers = [
+  AuthController,
+  AppointmentController,
 ];
 
 @Module({
@@ -36,9 +47,10 @@ const services = [
     }),
     InventoryModule,
     SupportRequestModule,
+    DonationModule,
     BeneficiaryModule,
   ],
-  controllers: [AuthController],
+  controllers: [...controllers],
   providers: [...repositories, ...services],
 })
 export class AppModule {}
