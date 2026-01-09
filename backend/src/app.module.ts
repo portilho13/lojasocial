7f0b885c967ca28c7f1e67efabb6ec9e838e5614
TIAGO\tiago <a27675@alunos.ipca.pt>
2026-01-09 01:38:40 +0000

    correction

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,40 +1,29 @@
 import { Module } from '@nestjs/common';
-import { JwtModule } from '@nestjs/jwt';
 import { AuthController } from './controller/auth.controller';
-import { AppointmentController } from './controller/appointment.controller';
 import { PrismaModule } from 'prisma/prisma.module';
+import { JwtModule } from '@nestjs/jwt';
 import { InventoryModule } from './inventory.module';
 import { SupportRequestModule } from './support-request.module';
 import { BeneficiaryModule } from './beneficiary.module';
-import { DonationModule } from './donation.module';
+
 import { StudentRepository } from './repository/student.repository';
 import { UserRepository } from './repository/user.repository';
-import { AppointmentsRepository } from './repository/appointments.repository';
+
 import { StudentService } from './service/student.service';
 import { UserService } from './service/user.service';
-import { AppointmentService } from './service/appointment.service';
-import { EmailService } from './service/email.service';
 import { AccessTokenStrategy } from './auth/strategies/access-token.strategy';
 import { RefreshTokenStrategy } from './auth/strategies/refresh-token.strategy';
 
 const repositories = [
   StudentRepository,
   UserRepository,
-  AppointmentsRepository,
 ];
 
 const services = [
   StudentService,
   UserService,
-  AppointmentService,
-  EmailService,
   AccessTokenStrategy,
   RefreshTokenStrategy,
-];
-
-const controllers = [
-  AuthController,
-  AppointmentController,
 ];
 
 @Module({
@@ -48,9 +37,8 @@ const controllers = [
     InventoryModule,
     SupportRequestModule,
     BeneficiaryModule,
-    DonationModule,
   ],
-  controllers: [...controllers],
+  controllers: [AuthController],
   providers: [...repositories, ...services],
 })
 export class AppModule {}
